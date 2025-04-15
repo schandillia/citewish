d4dc3d3750c703f79ccc9fe6a192c83d51db4c30
schandillia <amit@schandillia.com>
2025-04-15 21:31:18 +0530

    chore: improve hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,26 @@
-import type { Metadata } from "next";
-import { bodyFont, accentFont } from "@/lib/fonts";
-import meta from "@/lib/meta.json";
+import type { Metadata } from "next"
+import { bodyFont, accentFont } from "@/lib/fonts"
+import meta from "@/lib/meta.json"
 
-import "./globals.css";
-import { cn } from "@/lib/utils";
+import "./globals.css"
+import { cn } from "@/lib/utils"
 
 export const metadata: Metadata = {
   title: meta.HOME.TITLE,
   description: meta.HOME.DESCRIPTION,
   icons: [{ rel: "icon", url: "/favicon.ico" }],
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className={cn(bodyFont.variable, accentFont.variable)}>
-      <body className="font-sans antialiased">{children}</body>
+      <body className="min-h-[calc(100vh-1px)] flex flex-col font-sans text-brand-950 antialiased">
+        <main className="relative flex-1 flex flex-col">{children}</main>
+      </body>
     </html>
-  );
+  )
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link"
+import { MaxWidthWrapper } from "@/components//max-width-wrapper"
+import { Button, buttonVariants } from "@/components/ui/button"
+// import { auth } from "@/auth"
+import { BrandLogo } from "@/components/brand-logo"
+// import { DashboardNavButton } from "@/components/dashboard-nav-button"
+// import UserNavMenu from "@/components/user-nav-menu"
+// import { AuthButton } from "@/components/auth/auth-button"
+
+export const Navbar = async () => {
+  //   const session = await auth()
+
+  const guestLinks: {
+    href: string
+    label: string
+    variant:
+      | "default"
+      | "destructive"
+      | "outline"
+      | "secondary"
+      | "ghost"
+      | "link"
+  }[] = [
+    { href: "/pricing", label: "Pricing", variant: "link" },
+    { href: "/test", label: "Test", variant: "link" },
+  ]
+
+  return (
+    <nav className="sticky z-50 h-16 inset-x-0 top-0 w-full border-b border-gray-200 dark:border-gray-800 bg-white/80 dark:bg-brand-900/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link href="/" className="flex z-40 font-semibold">
+            <BrandLogo />
+          </Link>
+
+          <div className="h-full flex items-center space-x-4">
+            {guestLinks.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className={`${buttonVariants({
+                  size: "sm",
+                  variant: link.variant,
+                })} dark:text-brand-50`}
+              >
+                {link.label}
+              </Link>
+            ))}
+            <div className="h-8 w-px bg-gray-200" />
+            <Button size="sm" variant="brand">
+              Login
+            </Button>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  )
+}
