d4d8aa470c30b86148eb72ed943f80242883c8c0
schandillia <amit@schandillia.com>
2025-04-16 19:10:16 +0530

    added routes

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -2,7 +2,7 @@
 
 import { signIn } from "@/auth"
 import { signOut } from "@/auth"
-// import { publicRoutes } from "@/routes"
+import { publicRoutes } from "@/routes"
 
 export async function signInWithGoogle(callbackUrl?: string) {
   await signIn("google", {
@@ -11,8 +11,8 @@ export async function signInWithGoogle(callbackUrl?: string) {
 }
 
 export async function logOut(currentPath: string) {
-  //   const isPublicRoute = publicRoutes.has(currentPath) // Changed from includes() to has()
+  const isPublicRoute = publicRoutes.has(currentPath) // Changed from includes() to has()
   await signOut({
-    // redirectTo: isPublicRoute ? currentPath : "/",
+    redirectTo: isPublicRoute ? currentPath : "/",
   })
 }
